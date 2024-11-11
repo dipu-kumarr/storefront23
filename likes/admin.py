1d20d87930a6feabf1b5c81bd9dadb6e9b5f537b
Dipu kumar <105379708+dipu-kumarr@users.noreply.github.com>
2024-11-12 01:49:34 +0530

    hjsdfkhsjksdf

--- likes/admin.py
+++ likes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from rest_framework_nested.routers import NestedSimpleRouter
 # Register your models here.
 from django.urls import path
 from django.urls.conf import include
@@ -14,10 +14,10 @@ router.register ('collections', views.CollectionViewSet)
 router.register ('carts',views.CartViewSet)
 
 
-products_router = routers.NestedDefaultRouter(router,'products',lookup='product')
+products_router = routers.NestedSimpleRouter(router,'products',lookup='product')
 products_router.register('reviews',views.ReviewViewSet,basename = 'product-reviews')
 
-carts_router = routers.NestedDefaultRouter(router,'carts',lookup='cart')
+carts_router = routers.NestedSimpleRouter(router,'carts',lookup='cart')
 carts_router.register('items',views.CartItemViewSet, basename='cart-items-detail')
 # URLConf
 urlpatterns = router.urls + products_router.urls + carts_router.urls
